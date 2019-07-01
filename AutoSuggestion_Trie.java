ddf5cbb3bbd357b5c0c96df573a565e16ca00056
Kei Fukutani <kifinewera@gmail.com>
2019-06-30 20:57:01 -0700

    cleaned tabs.

--- AutoSuggestion_Trie.java
+++ AutoSuggestion_Trie.java
@@ -3,9 +3,9 @@
 // Date  : June 30, 2019
 public class AutoSuggestion_Trie {
 
-	public static void main(String[] args) {		
-		
-		// Build Trie. 
+    public static void main(String[] args) {        
+        
+        // Build Trie. 
         Trie trie = new Trie();
         trie.insert("aa");
         trie.insert("abc");
@@ -13,48 +13,48 @@ public class AutoSuggestion_Trie {
         trie.insert("abbbaba");
         
         // Test printAutoSuggestions(). 
-		trie.printAutoSuggestions(trie.root, "ab");
-//		abbbaba
-//		abc
-//		abcd
-		System.out.println();
-		trie.printAutoSuggestions(trie.root, "abc");
-//		abc
-//		abcd
-		System.out.println();
-		trie.printAutoSuggestions(trie.root, ""); // Enter a non-empty string.
-		System.out.println();
-		trie.printAutoSuggestions(trie.root, "ad"); // No string found.
-		System.out.println();
-		trie.printAutoSuggestions(trie.root, "aa"); // aa
-		System.out.println();
-		
-		trie = new Trie();
-		trie.insert("hello"); 
-		trie.insert("dog"); 
-		trie.insert("hell"); 
-		trie.insert("cat"); 
-		trie.insert("a"); 
-		trie.insert("hel"); 
-		trie.insert("help"); 
-		trie.insert("helps"); 
-		trie.insert("helping"); 
-	    trie.printAutoSuggestions(trie.root, "hel"); 
-	    System.out.println();
-//	    hel
-//	    hell
-//	    hello
-//	    help
-//	    helping
-//	    helps
-		
-	    
-	    
-		
+        trie.printAutoSuggestions(trie.root, "ab");
+//      abbbaba
+//      abc
+//      abcd
+        System.out.println();
+        trie.printAutoSuggestions(trie.root, "abc");
+//      abc
+//      abcd
+        System.out.println();
+        trie.printAutoSuggestions(trie.root, ""); // Enter a non-empty string.
+        System.out.println();
+        trie.printAutoSuggestions(trie.root, "ad"); // No string found.
+        System.out.println();
+        trie.printAutoSuggestions(trie.root, "aa"); // aa
+        System.out.println();
+        
+        trie = new Trie();
+        trie.insert("hello"); 
+        trie.insert("dog"); 
+        trie.insert("hell"); 
+        trie.insert("cat"); 
+        trie.insert("a"); 
+        trie.insert("hel"); 
+        trie.insert("help"); 
+        trie.insert("helps"); 
+        trie.insert("helping"); 
+        trie.printAutoSuggestions(trie.root, "hel"); 
+        System.out.println();
+//      hel
+//      hell
+//      hello
+//      help
+//      helping
+//      helps
+        
+        
+        
+        
 
-	}
-	
-	
+    }
+    
+    
 
 }
 
@@ -135,80 +135,59 @@ class Trie {
     
     // Print suggestions for given query prefix. 
     public int printAutoSuggestions(TrieNode root, String query) {
-    	if (query.isEmpty()) {
-    		System.out.println("Enter a non-empty string.");
-			return 0;
-		}
-    	
-    	// Get the last node of the query. 
-    	TrieNode lastNode = this.getLastNode(query);
-    	if (lastNode == null) {
-    		// There is no string that starts with this query. 
-    		System.out.println("No string found.");
-			return 0;
-		}
-    	
-    	if (lastNode.isEnd() && lastNode.getNumChild() == 0) {
-    		// The query is present as a word, and there is no subtree
-    		// below the last node. 
-    		System.out.println(query);
-			return -1;
-		}
-    	
-    	if (lastNode.getNumChild() != 0) {
+        if (query.isEmpty()) {
+            System.out.println("Enter a non-empty string.");
+            return 0;
+        }
+        
+        // Get the last node of the query. 
+        TrieNode lastNode = this.getLastNode(query);
+        if (lastNode == null) {
+            // There is no string that starts with this query. 
+            System.out.println("No string found.");
+            return 0;
+        }
+        
+        if (lastNode.isEnd() && lastNode.getNumChild() == 0) {
+            // The query is present as a word, and there is no subtree
+            // below the last node. 
+            System.out.println(query);
+            return -1;
+        }
+        
+        if (lastNode.getNumChild() != 0) {
             StringBuilder prefix = new StringBuilder(query);
             suggestionsRecur(lastNode, prefix);
-		}
-    	
-		return 1;
+        }
+        
+        return 1;
     }
 
     // Print auto-suggestions for given prefix. 
     // @param node  the last node in the prefix. 
     private void suggestionsRecur(TrieNode node, StringBuilder prefix) {
-		if (node.isEnd()) {
+        if (node.isEnd()) {
             System.out.println(prefix.toString());
-		}
-		if (node.getNumChild() == 0) {
-			// No child node. 
-			return;
-		}
-		
-		for (int i = 0; i < 26; i++) {
-			char c = (char) (i + 'a');
-			if (node.containsKey(c)) {	
+        }
+        if (node.getNumChild() == 0) {
+            // No child node. 
+            return;
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            char c = (char) (i + 'a');
+            if (node.containsKey(c)) {  
                 StringBuilder curPrefix = new StringBuilder(prefix);
                 curPrefix.append(c);
                 suggestionsRecur(node.get(c), curPrefix);
-			}	
-		}
-	}
+            }   
+        }
+    }
     
     
     
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
